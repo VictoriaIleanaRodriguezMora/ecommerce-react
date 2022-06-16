7cbc9b7f628742e61f153ab179b2883c74884e6e
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-06-16 10:32:56 -0300

    Animation scroll

--- ecommerce-react/src/App.js
+++ ecommerce-react/src/App.js
@@ -3,18 +3,20 @@ import Header from './components/Header/Header';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ScrollAnimation from './components/ScrollAnimation/ScrollAnimation';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <ScrollAnimation/>
+      {/* <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/" element={<ItemListContainer />, } />
           <Route path="/detail/:productId" element={<ItemDetailContainer />} />
           <Route path="/category/:categoryId" element={<ItemListContainer />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 }

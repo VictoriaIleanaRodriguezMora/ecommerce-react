57bc81f2f8404d9592dc63020a1bf6e24caa1819
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-06-18 21:01:26 -0300

    Configuraciones iniciales de Firebase

---
+++ ecommerce-react/src/services/index.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC304Lv36kXduaQEUoE2sjWod-r9Wpfgis",
+  authDomain: "ecommerce-react-app-a71ac.firebaseapp.com",
+  projectId: "ecommerce-react-app-a71ac",
+  storageBucket: "ecommerce-react-app-a71ac.appspot.com",
+  messagingSenderId: "1067785952262",
+  appId: "1:1067785952262:web:6e752bd89d59bb31a3b787"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const bdd =  getFirestore(app)

b176abf61128e6ce1e3430ac77c59de5a5dfdfbd
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-06-21 19:35:44 -0300

    Tabla de productos en el carrito

--- ecommerce-react/src/components/CartItem/CartItem.js
+++ ecommerce-react/src/components/CartItem/CartItem.js
@@ -18,17 +18,40 @@ const CartItem = () => {
     const cartPosCero = cart[0]
     console.log(cartPosCero.id);
     return (
-        <>
             <div className="CartItem__div">
-                <ul className="CartItem__ul">
+                <div  >
+                    <table className="table">
+                        <thead>
+                            <tr className="CartItem__categorias">
+                                <th scope="col">#</th>
+                                <th scope="col">Productos</th>
+                                <th scope="col">Cantidad</th>
+                                <th scope="col">Precio</th>
+                                <th scope="col">Total</th>
+                                <th scope="col">Remover</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="CartItem__categorias">
+                                <th scope="row"  >1</th>
+                                <td>{cartPosCero.name}</td>
+                                <td>{cartPosCero.quant}</td>
+                                <td>{cartPosCero.price}</td>
+                                <td>{getTotalPrice()}</td>
+                                <td onClick={() => removeProdFromCart(cartPosCero.id)} >Remover item</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+                {/* <ul className="CartItem__ul">
                     <li>{cartPosCero.name}</li>
                     <li>{cartPosCero.quant}</li>
                     <li>{cartPosCero.price}</li>
                     <li>{getTotalPrice()}</li>
-                </ul>
-                <button onClick={ () => removeProdFromCart(cartPosCero.id) } >Remover item</button>
+                    <button onClick={() => removeProdFromCart(cartPosCero.id)} >Remover item</button>
+                </ul> */}
             </div>
-        </>
     )
 
 }

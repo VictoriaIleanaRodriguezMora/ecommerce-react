b20a64c5bbf9628b6e64ac163debe84bec6a6869
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-07-18 15:10:43 -0300

    Efecto correcto en navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -12,7 +12,7 @@ const NavBar = () => {
 
     return (
         <>
-            <nav className={`nav ${menuHamb ? 'active' : '' }`} >
+            <nav className={`nav ${menuHamb ? 'activeNav' : '' }`} >
                 <Link to="/" >
                     <h3 className="nav__marca" >Marca</h3>
                 </Link>
@@ -27,7 +27,7 @@ const NavBar = () => {
                 </Link>
 
             </nav>
-            <button onClick={fnMenuHamburguesa} ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" >
+            <button onClick={fnMenuHamburguesa} ><svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" >
                 <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
             </svg></button>
         </>
